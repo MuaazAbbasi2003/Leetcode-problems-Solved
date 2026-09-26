5c2b9c37969a73d0394f4c14108f9169d8a082ec
Muaaz Abbasi <muaazabbasi2003@gmail.com>
2026-09-26 19:21:39 +0500

    //Reverse Words in a String

--- leetCode/problem6.js
+++ leetCode/problem6.js
@@ -1,1 +1,27 @@
-151; //Reverse Words in a String
+//Reverse Words in a String
+// var reverseWords = function (s) {
+//   let word = "";
+//   let array1 = [...s];
+//   let arr1 = [];
+//   for (let index = 0; index < array1.length; index++) {
+//     const element = array1[index];
+//     if (index === array1.length - 1 && element !== " ") {
+//       word += element;
+//       arr1.push(word);
+//     }
+//     if (element === " " && word.length !== 0) {
+//       arr1.push(word);
+//       word = "";
+//     } else if (element !== " ") {
+//       word += element;
+//     }
+//   }
+//   return arr1.reverse().join(" ");
+// };
+var reverseWords = function (s) {
+  return s.split(" ").filter(Boolean).reverse().join(" ");
+};
+
+const s = "the sky is blue";
+const hel = reverseWords(s);
+console.log(hel);
